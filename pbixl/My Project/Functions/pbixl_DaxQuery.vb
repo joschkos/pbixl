a1313cff20bd813f6b847978025cfc8e28f38b4f
joschkos <joerg.schmidt@sysaccount.de>
2023-10-07 13:31:28 +0200

    update

--- pbixl/My Project/Functions/pbixl_DaxQuery.vb
+++ pbixl/My Project/Functions/pbixl_DaxQuery.vb
@@ -6,12 +6,18 @@ Public Module pbixl_DaxQuery
 
 
     <ExcelFunction(Description:="pbixl", Category:="pbixl", IsMacroType:=False, IsVolatile:=True, IsHidden:=False, Name:="pbixl.DAX")>
-    Public Function pbixl_Query(ConnName As Object, Statement As Object, Hint As Object) As Object
+    Public Function pbixl_DaxQuery(ConnName As Object, Statement As Object, Hint As Object) As Object
+
+        Return "Hello My Friend" & Now.ToLongTimeString
 
 
-        Return "Hello my Friend" & Now.ToLongTimeString
+    End Function
 
 
+    <ExcelFunction(Description:="pbixl", Category:="pbixl", IsMacroType:=False, IsVolatile:=True, IsHidden:=False, Name:="pbixl.SQL")>
+    Public Function pbixl_SqlQuery(ConnName As Object, Statement As Object, Hint As Object) As Object
+
+        Return "Hello My Friend" & Now.ToLongTimeString
 
 
     End Function
